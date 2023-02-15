06a951ca4f46fbfda581b56355a0cf24f73521ab
Atharva123987 <atharva.amberkar@gmail.com>
2023-02-15 18:27:25 +0530

    Carousel fixed

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -9,13 +9,13 @@ const Slider =()=> {
     <Carousel fade>
       <Carousel.Item>
         <img
-          className=""
+          className="slider-img"
           src={Car1}
           alt="First slide"
           style={{width:"100%"}}
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
+          <h3 class="headings">First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
@@ -28,7 +28,7 @@ const Slider =()=> {
         />
 
         <Carousel.Caption>
-          <h3>Second slide label</h3>
+          <h3 class="headings">Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
@@ -41,7 +41,7 @@ const Slider =()=> {
         />
 
         <Carousel.Caption>
-          <h3>Third slide label</h3>
+          <h3 class="headings">Third slide label</h3>
           <p>
             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
           </p>
