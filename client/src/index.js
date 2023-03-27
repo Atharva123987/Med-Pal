5c7244ef9f77fc47ac5df0a037bdb1b7108f4d23
Atharva123987 <atharva.amberkar@gmail.com>
2023-03-27 10:31:07 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
