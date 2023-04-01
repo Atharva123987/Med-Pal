13dc2065b18ff323517fd56b0587a834c60765c4
Atharva123987 <atharva.amberkar@gmail.com>
2023-04-01 18:24:02 +0530

    Update CarouselData.jsx

--- client/src/assets/CarouselData.jsx
+++ client/src/assets/CarouselData.jsx
@@ -2,14 +2,14 @@
 export const CarouselData = [
     {
       image:
-        'https://ik.imagekit.io/0qlf5pqwx/carousel1.webp?updatedAt=1680352587445'
+        'https://ik.imagekit.io/0qlf5pqwx/abstract-background-illustration-watercolor-blue-green__1_.webp?updatedAt=1680353538152'
     },
     {
         image:
-         'https://ik.imagekit.io/0qlf5pqwx/carousel1.webp?updatedAt=1680352587445'
+         'https://ik.imagekit.io/0qlf5pqwx/abstract-background-illustration-watercolor-blue-green__1_.webp?updatedAt=1680353538152'
       },
       {
         image:
-          'https://ik.imagekit.io/0qlf5pqwx/carousel1.webp?updatedAt=1680352587445'
+          'https://ik.imagekit.io/0qlf5pqwx/abstract-background-illustration-watercolor-blue-green__1_.webp?updatedAt=1680353538152'
       },
   ];
