f1560b8b8128bbd375164a876bedad333a2bfc04
Aastle Stephno <stephno2003@gmail.com>
2023-04-01 13:35:58 +0530

    Models and initial test Medicine schema model and inital post test done. Other model files created.

--- server/routes/medicines.js
+++ server/routes/medicines.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const Medicine = require("../models/medicinesModel");
 
 const router = express.Router();
 
@@ -13,8 +14,22 @@ router.get("/:id", (req, res) => {
 });
 
 // POST a new medicine
-router.post("/", (req, res) => {
-	res.json({ mssg: "POST a new medicine" });
+router.post("/", async (req, res) => {
+	const { name, quantity, expiry, frequency, timeOfDay, dosageEndDate } =
+		req.body;
+	try {
+		const newMedicine = await Medicine.create({
+			name,
+			quantity,
+			expiry,
+			frequency,
+			timeOfDay,
+			dosageEndDate,
+		});
+		res.status(200).json({ mssg: "POST a new medicine", newMedicine });
+	} catch (err) {
+		res.status(400).json({ error: err.message });
+	}
 });
 
 // DELETE a medicine
