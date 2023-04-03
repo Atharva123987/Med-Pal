00faf92b5ad0df10e1075ec8893563ef1090df5b
Atharva123987 <atharva.amberkar@gmail.com>
2023-04-03 19:34:14 +0530

    Update Admin.jsx

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -2,7 +2,9 @@ import { useEffect, useState } from "react";
 import axios from 'axios'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-
+import Toast from 'react-bootstrap/Toast';
+import {FaHospital} from 'react-icons/fa'
+import {FaMoneyBill} from 'react-icons/fa'
 const Admin = () => {
     const [doctorName, setDoctorName] = useState('')
     const [speciality, setSpeciality] = useState('')
@@ -11,9 +13,15 @@ const Admin = () => {
     const [address, setAddress] = useState('')
     const [fees, setFees] = useState(0)
     const [fetchedData, setFetchedData] = useState(null)
+    const [show, setShow] = useState(false)
+    const [error, setError] = useState(false)
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        setShow(true)
+        setError(true)
+
+        // !!!HANDLE POST REQUEST HERE
         console.log(
                 doctorName, 
                 speciality, 
@@ -26,6 +34,7 @@ const Admin = () => {
 
     const fetchData = async (e) => {
         e.preventDefault();
+        // !!!HANDLE GET REQUEST HERE
         try {
             const response = await axios.get(`https://random-data-api.com/api/v2/users?size=5`)
             setFetchedData(response.data)
@@ -39,6 +48,39 @@ const Admin = () => {
 
     return (
         <>
+        <div id='toasts' style={{ position: "fixed", zIndex: "10", top: "3%", right: "3%" }}>
+                    <Toast onClose={() => setShow(false)} bg='success' position='middle-center' show={show} delay={3000} autohide style={{ position: "fixed", zIndex: "10", top: "3%", right: "3%" }}>
+                        <Toast.Header>
+                            <img
+                                src="holder.js/20x20?text=%20"
+                                className="rounded me-2"
+                                alt=""
+                            />
+                            <strong className="me-auto">Doctor Added!</strong>
+                            <small>Doctor {doctorName?.charAt(0).toUpperCase() + doctorName?.slice(1)}</small>
+                        </Toast.Header>
+                        <Toast.Body className="text-white"> 
+                        <FaHospital/>
+                            {clinicOrHospita1Name} <br></br>
+                        <FaMoneyBill/>
+                            {fees}
+                        </Toast.Body>
+                    </Toast>
+
+                    <Toast onClose={() => { setError(false) }} bg='danger' position='middle-center' show={error} delay={2000} autohide style={{ position: "relative", zIndex: "10" }}>
+                        <Toast.Header>
+                            <img
+                                src="holder.js/20x20?text=%20"
+                                className="rounded me-2"
+                                alt=""
+                            />
+                            <strong className="me-auto text-danger">Enter valid details!</strong>
+
+                        </Toast.Header>
+                        <Toast.Body className='text-white'>Check all the details before submitting</Toast.Body>
+                    </Toast>
+                </div>
+
             <h1>Admin Page for Doctors</h1>
             <div className='d-flex justify-content-evenly'>
                 <Form>
