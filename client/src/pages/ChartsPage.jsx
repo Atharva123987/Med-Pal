096a99c8bb58a1f88001098f30922da570e1b75a
Atharva123987 <atharva.amberkar@gmail.com>
2023-04-05 20:51:28 +0530

    Sending fetched data as props to charts component

--- client/src/pages/ChartsPage.jsx
+++ client/src/pages/ChartsPage.jsx
@@ -1,76 +1,118 @@
 import Navbar from "../components/Navbar";
 import AllCharts from '../components/Charts'
-import { Form } from "react-bootstrap";
+import { Form, Toast } from "react-bootstrap";
 import { useState, useRef, useEffect } from "react";
 import axios from "axios";
-// import AddMedicineModal from "../components/Medicines/AddMedicineModal";
+
 const Charts = () => {
 
     const [chartType, setChartType] = useState(null)
-    const [bSugar, setBSugar] = useState(null)
-    const [bPressure, setBPressure] = useState(null)
-    const [bHaemoglobin, setBHaemoglobin] = useState(null)
+    const [readingValue, setReadingValue] = useState(null)
     const [readingDate, setReadingDate] = useState(null)
-    const [payLoadData, setPayLoadData] = useState(null)
-    const [fetchedData, setFetchedData] = useState(null)
+    const [showToast, setShowToast] = useState(false);
+    const [showError, setShowError] = useState(false);
+    const [countValue, setCountValue] = useState(null)
+    const [fetchedData, setFetchedData] = useState([[
+        {
+                date: new Date(2020, 6, 1).toISOString().slice(0, 10),
+                count: 130,
+            },
+            {
+                date: new Date(2021, 5, 1).toISOString().slice(0, 10),
+                count: 120,
+            },
+            {
+                date: new Date(2021, 7, 1).toISOString().slice(0, 10),
+                count: 190,
+            },
+            {
+                date: new Date(2021, 8, 1).toISOString().slice(0, 10),
+                count: 300,
+            },
+            {
+                date: new Date(2022, 12, 1).toISOString().slice(0, 10),
+                count: 40,
+            }
+        ],
+        [
+            {
+                date: new Date(2020, 6, 1).toISOString().slice(0, 10),
+                count: 130,
+            },
+            {
+                date: new Date(2021, 5, 1).toISOString().slice(0, 10),
+                count: 220,
+            },
+            {
+                date: new Date(2021, 7, 1).toISOString().slice(0, 10),
+                count: 250,
+            },
+            {
+                date: new Date(2021, 8, 1).toISOString().slice(0, 10),
+                count: 300,
+            },
+            {
+                date: new Date(2022, 12, 1).toISOString().slice(0, 10),
+                count: 100,
+            },
+        ],
+        [
+            {
+                date: new Date(2020, 6, 1).toISOString().slice(0, 10),
+                count: 6,
+            },
+            {
+                date: new Date(2021, 5, 1).toISOString().slice(0, 10),
+                count: 17,
+            },
+            {
+                date: new Date(2021, 7, 1).toISOString().slice(0, 10),
+                count: 3,
+            },
+            {
+                date: new Date(2021, 8, 1).toISOString().slice(0, 10),
+                count: 6,
+            },
+            {
+                date: new Date(2022, 12, 1).toISOString().slice(0, 10),
+                count: 19,
+            },
+        ]
+    ])
 
-    useEffect(()=>{
+    useEffect(() => {
         handleFetch();
-    },[])
+        // console.log(fetchedData)
+    }, [])
 
 
     const handleSubmit = async (e) => {
         e.preventDefault();
 
         // !!!POST REQUEST HERE
-        // INSTEAD OF CONSOLE.LOG IN SWITCH, SET THE PAYLOAD DATA USING setPayLoadData() AND THEN SEND IT USING POST REQUEST
-
-       
-
-        switch (chartType) {
-            case 'bSugar':
-                console.log(bSugar)
-                break;
-            case 'bPressure':
-                console.log(bPressure)
-                break;
-            case 'bHaemoglobin':
-                console.log(bHaemoglobin)
-                break;
-            default:
-                console.log("")
-
-        }
+        // USE chartType TO DETERMINE TO WHICH ENDPOINT THE POST REQUEST IS TO BE MADE
+        console.log(chartType);
+        console.log(readingValue)
         console.log(readingDate.toISOString())
+
+        setShowToast(true);
+
     }
 
-    const handleFetch = async()=>{
-        try{
+    const handleFetch = async () => {
+        try {
             // !!!GET REQUEST GOES HERE
-            const response = axios.get(``)
-            setFetchedData(response.data);
+            // const response = axios.get(``)
+            // const response = axios.get(``)
+            // const response = axios.get(``)
+            // setFetchedData({response1.data},{response2.data},{response3.data});
         }
-        catch(err){
+        catch (err) {
             console.log(err);
+            setShowError(true);
         }
     }
 
-    const handleValue = (e) => {
-        switch (chartType) {
-            case 'bSugar':
-                setBSugar(e.target.value)
-                break;
-            case 'bPressure':
-                setBPressure(e.target.value)
-                break;
-            case 'bHaemoglobin':
-                setBHaemoglobin(e.target.value)
-                break;
-            default:
-                console.log("Choose a chart type!!!")
-                e.target.value = ''
-        }
-    }
 
     return (
         <>
@@ -80,7 +122,6 @@ const Charts = () => {
                 <div>
                     <Form>
                         <Form.Select aria-label="Default select example" onChange={(e) => {
-                            console.log(e.target.value)
                             setChartType(e.target.value)
                         }}>
                             <option>Select a chart</option>
@@ -91,7 +132,7 @@ const Charts = () => {
 
                         <Form.Group className="mb-3" controlId="exampleForm.ControlInput2" style={{ width: "300px" }}>
                             <Form.Label>Count value</Form.Label>
-                            <Form.Control type="number" placeholder="Quantity" onChange={handleValue} />
+                            <Form.Control type="number" placeholder="Count value" onChange={(e)=>setReadingValue(e.target.value)} />
                         </Form.Group>
 
                         <Form.Group
@@ -119,9 +160,34 @@ const Charts = () => {
 
                     </Form>
                 </div>
-                <AllCharts chartData={fetchedData}/>
+                <AllCharts chartData={fetchedData} />
 
-                {/* <AddMedicineModal/> */}
+                <div id='toasts' style={{ position: "fixed", zIndex: "10", top: "3%", right: "3%" }}>
+                <Toast onClose={() => { setShowToast(false) }} bg='success' position='middle-center' show={showToast} delay={3000} autohide style={{ position: "relative", zIndex: "10" }}>
+                    <Toast.Header>
+                        <img
+                            src="holder.js/20x20?text=%20"
+                            className="rounded me-2"
+                            alt=""
+                        />
+                        <strong className="me-auto text-success">Value Added!</strong>
+                        {/* <small>{bSugar}</small> */}
+                    </Toast.Header>
+                    <Toast.Body className='text-white'></Toast.Body>
+                </Toast>
+
+                <Toast onClose={() => { setShowError(false) }} bg='danger' position='middle-center' show={showError} delay={2000} autohide style={{ position: "relative", zIndex: "10" }}>
+                    <Toast.Header>
+                        <img
+                            src="holder.js/20x20?text=%20"
+                            className="rounded me-2"
+                            alt=""
+                        />
+                        <strong className="me-auto text-danger">Enter Valid Name!</strong>
+                    </Toast.Header>
+                    <Toast.Body className='text-white'>Tablet name should be unique</Toast.Body>
+                </Toast>
+                </div>
             </div >
         </>
     )
