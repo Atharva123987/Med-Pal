630e0a448109ab53ac0270504d168ef7e1db7722
Atharva123987 <atharva.amberkar@gmail.com>
2023-04-08 02:17:12 +0530

    Update Search.jsx

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -5,6 +5,7 @@ import axios from "axios";
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import './search.css'
+import {Card} from 'react-bootstrap'
 
 const Search = () => {
 	const [searchQuery, setSearchQuery] = useState("");
