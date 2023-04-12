438b33725d8464ee5f6ad60e87f7ca916802969a
Aastle Stephno <stephno2003@gmail.com>
2023-04-13 01:35:15 +0530

    Base AuthContext config

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.js';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import { AuthContextProvider } from "./context/AuthContext.js";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
-root.render(<App />);
+root.render(
+	<React.StrictMode>
+		<AuthContextProvider>
+			<App />
+		</AuthContextProvider>
+	</React.StrictMode>
+);
