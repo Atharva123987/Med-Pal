cfe28bf01e1b8a7b156cd4d0b2fe1c03cd766a5b
Aastle Stephno <stephno2003@gmail.com>
2023-04-13 15:43:19 +0530

    Logout Hook and Auth State Update

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,31 +1,52 @@
 import React from "react";
-import './Navbar.css';
-import {Link} from 'react-router-dom';
-import Log_In from '../assets/login.png'
-import Register from '../assets/register.png'
-import Logo from '../assets/logo.png'
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+import Log_In from "../assets/login.png";
+import Register from "../assets/register.png";
+import Logo from "../assets/logo.png";
+import { useLogout } from "../hooks/useLogout";
 
 const Navbar = (props) => {
-  return (
-    <>
-      <div className="nav-container">
-          
-          <a href="/"><img src={Logo} className="logo" alt="website logo" /></a>
-          <div className="heading">MEDPAL</div>
-            <div className="navbar-buttons">
-              {
-                props.buttons && 
-                (
-                <>
-                <Link to={'/login'}><img src={Log_In} id='login-button'alt="login"/></Link>
-                <Link to={'/register'}><img src={Register}  id='register-button' alt="register"/></Link>
-                </>
-                )
-              }
-              
-            </div>
-        </div>
-    </>
-  );
+	const { logout } = useLogout();
+
+	const handleClick = () => {
+		logout();
+	};
+
+	return (
+		<>
+			<div className="nav-container">
+				<a href="/">
+					<img src={Logo} className="logo" alt="website logo" />
+				</a>
+				<div className="heading">MEDPAL</div>
+				<div className="navbar-buttons">
+					<div>
+						<button onClick={handleClick}>Log out</button>
+					</div>
+					<div>
+						{props.buttons && (
+							<>
+								<Link to={"/login"}>
+									<img
+										src={Log_In}
+										id="login-button"
+										alt="login"
+									/>
+								</Link>
+								<Link to={"/register"}>
+									<img
+										src={Register}
+										id="register-button"
+										alt="register"
+									/>
+								</Link>
+							</>
+						)}
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
 export default Navbar;
