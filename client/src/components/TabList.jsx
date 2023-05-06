dbcf2c6c11d83ec5f60ea371e2d99a0835cc28f1
Atharva123987 <atharva.amberkar@gmail.com>
2023-05-06 19:59:52 +0530

    Ok

--- client/src/components/TabList.jsx
+++ client/src/components/TabList.jsx
@@ -1,66 +1,105 @@
-const TabList = () => {
-	const columns = [
-		{
-			name: "Crocin",
-			freq: "Daily",
-			time: ["morning", "evening"],
-			// selector: row => row.name,
-			// sector: row => row.nativeName,
-		},
-		{
-			name: "Norflox",
-			freq: "Daily",
-			time: ["morning", "evening"],
-			// selector: row => row.name,
-			// sector: row => row.nativeName,
-			// sortable:true,
-		},
-		{
-			name: "Becousule",
-			freq: "Bi-weekly",
-			time: ["morning", "evening"],
-			// sector: row => row.capital,
-		},
-		{
-			name: "Paracetamol",
-			freq: "Daily",
-			time: ["morning", "evening"],
-			// sector: row => row.capital,
-		},
-	];
+import { useEffect, useState } from "react";
+import { Table } from "react-bootstrap";
+import { CgUnavailable } from "react-icons/cg";
+import { Link } from "react-router-dom";
 
-	return (
-		<>
-			<div id="tab-list" className="dash-component">
-				<legend align="center">Tablet List</legend>
+const TabList = (props) => {
+    const [fetchedMedicineData, setFetchedMedicineData] = useState(null);
 
-				<table>
-					<tr>
-						<th>Name</th>
-						<th>Frequency</th>
-						<th>Time</th>
-					</tr>
-					{columns.map((val, key) => {
-						return (
-							<tr key={key}>
-								<td>{val.name}</td>
-								<td>{val.freq}</td>
-								<td>
-									{val.time.map(
-										(elem, i, time) => time[i] + ", "
-									)}
-								</td>
-							</tr>
-						);
-					})}
-				</table>
-				<div className="dash-button-container">
-					<button className="dash-button">
-						<span>+</span>
-					</button>
-				</div>
-			</div>
-		</>
-	);
+    useEffect(() => setFetchedMedicineData(props.fetchedMedicineData), [props.fetchedMedicineData]);
+
+    return (
+        <>
+            <div id="tab-list" className="dash-component">
+                <legend align="center">Medicine Reminder</legend>
+                {fetchedMedicineData ? (
+                    <>
+                        <Table striped bordered hover>
+                            <tr>
+                                <th>Name</th>
+                                <th>Frequency</th>
+                                <th>Time</th>
+                            </tr>
+                            {fetchedMedicineData
+    .filter((element) => element?.frequency)
+    .map((element, key) => {
+        return (
+            <tr key={key}>
+                <td>{element.name}</td>
+                <td>
+                    {element.frequency}
+                </td>
+                <td>
+                    <ul id="meds-table-list">
+                        {element?.timeOfDay &&
+                            Object.values(
+                                element?.timeOfDay
+                            ).every(
+                                (val) => !val.yesOrNot
+                            ) ? (
+                                <CgUnavailable style={{ color: "black" }} />
+                        ) : (
+                            <>
+                                {element?.timeOfDay?.morning?.yesOrNot && (
+                                    <li>
+                                        Morning{" "}
+                                        {
+                                            element.timeOfDay
+                                                .morning
+                                                .yesOrNot
+                                        }
+                                    </li>
+                                )}
+                                {element?.timeOfDay?.afternoon?.yesOrNot && (
+                                    <li>
+                                        Afternoon{" "}
+                                        {
+                                            element.timeOfDay
+                                                .afternoon
+                                                .yesOrNot
+                                        }
+                                    </li>
+                                )}
+                                {element?.timeOfDay?.evening?.yesOrNot && (
+                                    <li>
+                                        Evening{" "}
+                                        {
+                                            element.timeOfDay
+                                                .evening
+                                                .yesOrNot
+                                        }
+                                    </li>
+                                )}
+                                {element?.timeOfDay?.night?.yesOrNot && (
+                                    <li>
+                                        Night{" "}
+                                        {
+                                            element.timeOfDay
+                                                .night
+                                                .yesOrNot
+                                        }
+                                    </li>
+                                )}
+                            </>
+                        )}
+                    </ul>
+                </td>
+            </tr>
+        );
+})}
+                        </Table>
+                        <div className="dash-button-container">
+                            <Link to={'/medicines'}> <button className="dash-button">
+                                <span>+</span>
+                            </button>
+							</Link>
+                        </div>
+                    </>
+                ) : (
+                    <p>Loading medicine data...</p>
+                )}
+            </div>
+        </>
+    );
 };
 export default TabList;
