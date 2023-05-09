8e8e11f92a65d18271886cc2dde1f7d4f07d47d3
Pankaj Singh Sirari <Pankajsirari222@gmail.com>
2023-05-09 10:13:41 +0530

    Tooltip added to Take medicine

--- client/src/components/TabStock.jsx
+++ client/src/components/TabStock.jsx
@@ -4,121 +4,135 @@ import { Button, Table } from "react-bootstrap";
 import { AiFillMinusCircle, AiFillPlusCircle } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import { useAuthContext } from "../hooks/useAuthContext";
+import { Tooltip, OverlayTrigger } from "react-bootstrap";
 
 const TabStock = (props) => {
-	const [fetchedMedicineData, setFetchedMedicineData] = useState(null);
-	const { user } = useAuthContext();
-	useEffect(() => setFetchedMedicineData(props.fetchedMedicineData), [props.fetchedMedicineData]);
+  const [fetchedMedicineData, setFetchedMedicineData] = useState(null);
+  const { user } = useAuthContext();
+  useEffect(
+    () => setFetchedMedicineData(props.fetchedMedicineData),
+    [props.fetchedMedicineData]
+  );
 
-	const scrollToTop = () => {
-		window.scrollTo({
-		  top: 0,
-		  behavior: "smooth"
-		});
-	  }
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
 
-	const handleDelete = async (deleteID) => {
-		const axios = require("axios");
+  const handleDelete = async (deleteID) => {
+    const axios = require("axios");
 
-		let config = {
-			method: "delete",
-			maxBodyLength: Infinity,
-			url:
-				"https://medpal-backend.onrender.com/api/medicines/" + deleteID,
-			headers: {
-				Authorization: `Bearer ${user.token}`,
-			},
-		};
+    let config = {
+      method: "delete",
+      maxBodyLength: Infinity,
+      url: "https://medpal-backend.onrender.com/api/medicines/" + deleteID,
+      headers: {
+        Authorization: `Bearer ${user.token}`,
+      },
+    };
 
-		axios
-			.request(config)
-			.then((response) => {
-				
-				props.handleFetch();
-			})
-			.catch((error) => {
-				console.log(error);
-			});
-	};
+    axios
+      .request(config)
+      .then((response) => {
+        props.handleFetch();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-	const handleEdit = async (e,id,quantity,name) => {
-        if(quantity === 1){
-            handleDelete(id);
-			props.setShowTaken(true);
-                props.setTabletName(name);
-            return;
-        }
+  const handleEdit = async (e, id, quantity, name) => {
+    if (quantity === 1) {
+      handleDelete(id);
+      props.setShowTaken(true);
+      props.setTabletName(name);
+      return;
+    }
 
-		e.preventDefault();
-		console.log("ID ",id)
-		console.log("QUANTITY ",quantity)
-		let data = JSON.stringify({
-            quantity:quantity-1,
-		});
+    e.preventDefault();
+    console.log("ID ", id);
+    console.log("QUANTITY ", quantity);
+    let data = JSON.stringify({
+      quantity: quantity - 1,
+    });
 
-		let config = {
-			method: "patch",
-			maxBodyLength: Infinity,
-			url: `https://medpal-backend.onrender.com/api/medicines/${id}`,
-			headers: {
-				"Content-Type": "application/json",
-				Authorization: `Bearer ${user.token}`,
-			},
-			data: data,
-		};
+    let config = {
+      method: "patch",
+      maxBodyLength: Infinity,
+      url: `https://medpal-backend.onrender.com/api/medicines/${id}`,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${user.token}`,
+      },
+      data: data,
+    };
 
-		axios
-			.request(config)
-			.then((response) => {
-				props.setShowTaken(true);
-                props.setTabletName(name);
-				props.handleFetch();
-			})
-			.catch((error) => {
-				console.log(error);
-				// setShowError(true);
-			});
-	};
+    axios
+      .request(config)
+      .then((response) => {
+        props.setShowTaken(true);
+        props.setTabletName(name);
+        props.handleFetch();
+      })
+      .catch((error) => {
+        console.log(error);
+        // setShowError(true);
+      });
+  };
 
+  return (
+    <>
+      <div id="tab-stock" className="dash-component">
+        <legend align="center">Medicine Inventory</legend>
 
-	return (
-		<>
-			<div id="tab-stock" className="dash-component">
-				<legend align="center">Medicine Inventory</legend>
+        <Table striped bordered hover>
+          <tr>
+            <th>Name</th>
+            <th>Stock</th>
+            <th>Expiry</th>
+            <th>Action</th>
+          </tr>
 
-				<Table striped bordered hover>
-					<tr>
-						<th>Name</th>
-						<th>Stock</th>
-						<th>Expiry</th>
-						<th>Action</th>
-					</tr>
+          {fetchedMedicineData?.map((val, key) => {
+            return (
+              <tr key={key}>
+                <td>{val.name}</td>
+                <td>{val.quantity}</td>
+                <td>{new Date(val.expiry).toLocaleDateString()}</td>
+                <td>
+                  <div>
+                    <OverlayTrigger
+                      placement="right"
+                      overlay={<Tooltip>Take Medicine</Tooltip>}
+                    >
+                      <Button
+                        onClick={(e) =>
+                          handleEdit(e, val._id, val.quantity, val.name)
+                        }
+                        variant="secondary"
+                      >
+                        <AiFillMinusCircle />
+                      </Button>
+                    </OverlayTrigger>
+                  </div>
+                </td>
+              </tr>
+            );
+          })}
+        </Table>
 
-					{fetchedMedicineData?.map((val, key) => {
-						return (
-							<tr key={key}>
-								<td>{val.name}</td>
-								<td>{val.quantity}</td>
-								<td>{new Date(val.expiry).toLocaleDateString()}</td>
-								<td>
-                                                <div>
-                                                <Button onClick={(e)=>handleEdit(e,val._id, val.quantity,val.name)} variant="secondary">
-                                                    <AiFillMinusCircle />
-                                                </Button>
-                                                </div>
-                                            </td>
-							</tr>
-
-						);
-					})}
-				</Table>
-
-				<div className="dash-button-container">
-				<Link to={'/medicines'}> <Button variant="info" onClick={scrollToTop}><AiFillPlusCircle color="white"/></Button> 
-                            </Link>
-				</div>
-			</div>
-		</>
-	);
+        <div className="dash-button-container">
+          <Link to={"/medicines"}>
+            {" "}
+            <Button variant="info" onClick={scrollToTop}>
+              <AiFillPlusCircle color="white" />
+            </Button>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 };
 export default TabStock;
