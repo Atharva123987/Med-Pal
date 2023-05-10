1053813fa29b8793cdd14a3befbe6f20aa11b2e6
Atharva123987 <atharva.amberkar@gmail.com>
2023-05-10 22:03:14 +0530

    Update Streaks.jsx

--- client/src/components/Streaks.jsx
+++ client/src/components/Streaks.jsx
@@ -1,17 +1,25 @@
+import { Button } from "react-bootstrap";
+import {BsFillPlusCircleFill} from 'react-icons/bs'
+import {AiFillDownCircle} from 'react-icons/ai'
+import { useState } from "react";
+
 const Streaks = () =>{
+
+    const [streakNumber, setStreakNumber] = useState(4);
+
+    
+
     return (
-        <>
-          <legend align="center">Streaks</legend>
-        <div id="component-name" className="dash-component"> {/* <== Change id of the div to the name of component  */}
-        
+     
+        <div id="streaks-container" className="dash-component d-flex flex-column justify-content-start  "> 
+
           
-          {/* <-- Add component specific info here --> */}
-            <h5>Coming soon...</h5>
-          <div className="dash-button-container">
-          {/* <button className='dash-button'><span>+</span></button> */}
-          </div>
+          
+            <h5>Current streak : <span style={{color:"green"}}>{streakNumber}</span></h5>
+          <p>Click here if you took your medicine today <AiFillDownCircle/></p>
+          <Button variant='success'><BsFillPlusCircleFill/></Button>
+
         </div>
-        </>
       )
 }
 export default Streaks;
